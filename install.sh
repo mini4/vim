40691df994827451aa30ee111ffd515c717c42d1
mini4 <mini4@topcat.localhost.com>
2015-08-10 10:33:53 +0300

    добавил uninstall.sh и переработал install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
-SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-VIMRC=${SCRIPTPATH}/.vimrc
-VIM=${SCRIPTPATH}/.vim
+VIMRC="${HOME}/.vimrc"
+VIM="${HOME}/.vim"
 
-VIM_LIB=${VIM}/bundle/vim_lib
-[[ -d ${VIM_LIB} ]] || git clone https://github.com/Bashka/vim_lib.git $VIM_LIB
-VIM_PLUGMANAGER=${VIM}/bundle/vim_plugmanager
-[[ -d ${VIM_PLUGMANAGER} ]] || git clone https://github.com/Bashka/vim_lib.git $VIM_PLUGMANAGER
-[[ -L ${VIMRC} ]] && ln -s ${VIMRC} ${HOME}/.vimrc
-[[ -L ${VIM} ]] && ln -s ${VIM} ${HOME}/.vim
+TARGET="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VIMRC_TARGET="${TARGET}/.vimrc"
+[[ ( ! -L "${VIMRC}" || "$(readlink ${VIMRC})" != "${VIMRC_TARGET}" ) && -f "${VIMRC}" ]] && mv "${VIMRC}" "${HOME}/.viMrc"
+[[ -L "${VIMRC}" ]] || ln -s "${VIMRC_TARGET}" "${VIMRC}"
+
+VIM_TARGET="${TARGET}/.vim"
+[[ ( ! -L "${VIM}" || "$(readlink ${VIM})" != "${VIM_TARGET}" ) && -d "${VIM}" ]] && mv "${VIM}" "${HOME}/.viM"
+[[ -L "${VIM}" ]] || ln -s "${VIM_TARGET}" "${VIM}"
+
+VIM_LIB="${VIM}/bundle/vim_lib"
+[[ -d "${VIM_LIB}" ]] || git clone "https://github.com/Bashka/vim_lib.git" "$VIM_LIB"
+
+VIM_PLUGMANAGER="${VIM}/bundle/vim_plugmanager"
+[[ -d "${VIM_PLUGMANAGER}" ]] || git clone "https://github.com/Bashka/vim_lib.git" "$VIM_PLUGMANAGER"
 
