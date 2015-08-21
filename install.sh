a14f756f075f8646d840adc2170e5bafbb538e5c
Минич <kosmini4@gmail.com>
2015-08-21 22:58:46 +0300

    Исправлен install.sh: создание необходимых директорий

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ VIMRC_TARGET="${TARGET}/.vimrc"
 [[ -L "${VIMRC}" ]] || ln -s "${VIMRC_TARGET}" "${VIMRC}"
 
 VIM_TARGET="${TARGET}/.vim"
+PATH_BUNDLE="${VIM_TARGET}/bundle"
+[[ -d "${VIM_TARGET}" || -d "${PATH_BUNDLE}" ]] || mkdir -p "${PATH_BUNDLE}"
 [[ ( ! -L "${VIM}" || "$(readlink ${VIM})" != "${VIM_TARGET}" ) && -d "${VIM}" ]] && mv "${VIM}" "${HOME}/.viM"
 [[ -L "${VIM}" ]] || ln -s "${VIM_TARGET}" "${VIM}"
 
